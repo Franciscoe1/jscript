6ef80196f6248bb5a476910925ec38c0e96ebf5a
cynthia <cynthiayolanda0206@hotmail.com>
2025-04-15 11:43:29 -0500

    v2

--- scripts.js
+++ scripts.js
@@ -3,3 +3,13 @@ var edad;
 prompt("¿Cual es tu edad? "+ edad)
 console.log(edad);
 document.writeln("Hola mundo desde Js");
+
+var variable1 =10;
+var variable2 =20;
+
+if(variable1> variable2){
+    document.writeln("Número 1 es mayor que numero 2 " + variable1);
+}else{
+        document.writeln("<br>");
+        document.writeln("número 2 es mayor que numero1 "+ variable2);
+}   
